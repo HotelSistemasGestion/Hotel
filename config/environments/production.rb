d3c342be9fd1d861205584242e207fe07c4b72a6
MatiasGonzalezRomero95 <matias.gonzalez95@hotmail.com>
2016-09-26 11:49:30 -0400

    configurando heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.serve_static_assets = true
 
   
 end
