0e4b4d2e4c25fcd7e039c532296bbbce5920a4d0
Fernando <fernandoliseras@gmail.com>
2016-10-08 15:25:56 -0300

    Agregado types_of_employees

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928005947) do
+ActiveRecord::Schema.define(version: 20161008182432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -226,6 +226,13 @@ ActiveRecord::Schema.define(version: 20160928005947) do
   end
 
   create_table "type_of_rooms", force: :cascade do |t|
+    t.string   "tipo"
+    t.string   "descripcion"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "types_of_employees", force: :cascade do |t|
     t.string   "tipo"
     t.string   "descripcion"
     t.datetime "created_at",  null: false
