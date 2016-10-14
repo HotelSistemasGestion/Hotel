10bee2875d49d52f440a4537169a126f7bca14a3
Fernando <fernandoliseras@gmail.com>
2016-10-14 19:15:47 -0300

    Retirado los modals de abm rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,11 @@
 class Room < ActiveRecord::Base
   belongs_to :type_of_room
   belongs_to :state
-  has_many :room_comforts
+  has_many :room_comforts, :dependent => :destroy
   validates :identificador, presence: true
   delegate :tipo, to: :type_of_room, prefix: true, allow_nil: true
   delegate :descripcion, to: :state, prefix: true, allow_nil: true
-  accepts_nested_attributes_for :room_comforts ,:allow_destroy => true
+  accepts_nested_attributes_for :room_comforts ,:allow_destroy => true, update_only: true
 
 
 end 
