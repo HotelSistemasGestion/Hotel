9c46ab96dc6d7d5a93c73ed9f37569283b736866
MatiasGonzalezRomero95 <matias.gonzalez95@hotmail.com>
2016-10-31 23:58:15 -0300

    Correcion de un error en el Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -103,9 +103,9 @@ TypesOfEmployee.create(tipo: "administrador", descripcion: "");
 TypesOfEmployee.create(tipo: "limpiadora", descripcion: "");
 TypesOfEmployee.create(tipo: "botones", descripcion: "");
 
-Employee.create(nombre: "Pedro", types_of_employee_id: 2,);
+Employee.create(nombre: "Pedro", types_of_employee_id: 2);
 
-#User.create(:email => "admin@gmail.com", :password => '123456', :password_confirmation => '123456',roles_mask:1);
+User.create(:email => "admin@gmail.com", :password => '123456', :password_confirmation => '123456',roles_mask:1);
 
 ReservationRequest.create(nombre: "Leo", apellido: "Messi", email: "messi@gmail.com", telefono: "0012", type_of_room_id: 3, cantidad_de_adultos: 1, cantidad_de_ninhos: 1, cantidad_de_familias: 1, check_in: (Date.today+1).to_s, check_out: (Date.today+7).to_s, comentarios: "");
 ReservationRequest.create(nombre: "Matias", apellido: "Gonzalez", email: "maedgoro@gmail.com", telefono: "0012", type_of_room_id: 3, cantidad_de_adultos: 1, cantidad_de_ninhos: 1, cantidad_de_familias: 1, check_in: (Date.today+1).to_s, check_out: (Date.today+7).to_s, comentarios: "");
