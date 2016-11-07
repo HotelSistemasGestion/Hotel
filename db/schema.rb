9a9320b7a975a114aaaa55058ecb9f82f776d3be
Fabrizio <fabrizio123450@gmail.com>
2016-11-07 15:44:12 -0300

    modificado reservas ahora guarda datos de presupuesto

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161104144522) do
-
+ActiveRecord::Schema.define(version: 20161104172550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -371,10 +369,10 @@ ActiveRecord::Schema.define(version: 20161104144522) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "nombre"
-    t.string   "apellido"
-    t.string   "check_in"
-    t.string   "check_out"
+    t.string   "email"
+    t.string   "dias"
     t.string   "type_of_room_id"
+    t.string   "total"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
