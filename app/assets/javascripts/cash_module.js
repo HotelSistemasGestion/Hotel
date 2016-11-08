950116cb18f24d64a2d96e0d4fcdf61a494edd8f
Miguel Carreras <carreras69miguel@gmail.com>
2016-11-08 04:14:39 -0300

    En el select de caja muestra solamente las cajas cerradas y en el select de cajero muestra solamente los empleados de tipo cajero #53

--- app/assets/javascripts/cash_module.js
+++ app/assets/javascripts/cash_module.js
@@ -1,12 +1,12 @@
 
-
+// cuando selecciono un valor de pago
 $(document).on("change", ".select", function(){
         var id = "#"+$(this).attr("id");
         var cheque =  "#"+$(this).parents().siblings(".cheque").attr("id");
         console.log(cheque);
         mostrarCheque(id,cheque);
     });
-
+// funcion para mostrar o ocultar campos ocultos
 function mostrarCheque(id,cheque){
 	if ($(id).val() == "2") {
     	$(cheque).show();
