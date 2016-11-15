79b476ea6d7851902c76950d0f99b0d123624a3d
Ariel Sanabria <arielsanabria27@gmail.com>
2016-11-15 12:31:01 -0300

    Agregue el formato de moneda local al index de facturas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def number_to_currency_py(number)
+  		number_to_currency(number, :unit => "Gs. ",
+  									precision: 0, 
+  									format: "%n %u",
+  								    :separator => ",", 
+  									:delimiter => ".")
+	end
 end
