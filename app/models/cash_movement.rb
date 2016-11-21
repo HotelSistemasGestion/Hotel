952eb531685b287c83a3c99db992960c830e41f4
Miguel Carreras <carreras69miguel@gmail.com>
2016-11-21 14:57:20 -0300

    Mejoras en las vistas del modulo y agruegado validaciones faltantes en los modelos

--- app/models/cash_movement.rb
+++ app/models/cash_movement.rb
@@ -12,7 +12,8 @@ class CashMovement < ActiveRecord::Base
 	 
 	accepts_nested_attributes_for :payment_types, allow_destroy: true , update_only: true
 
+	validates :opening_cash_id, presence: true
 	validates :client_id, presence: true
 	validates :type_of_cash_movement_id, presence: true
-	  
+	validates :monto_total, presence: true, length: {minimum: 4, maximum: 12, :message => "4 dígitos minimo; 12 máximo."}, :numericality => {:only_integer => true, :message => "Solo puede ingresar numeros enteros"}	  
 end
--- app/models/opening_cash.rb
+++ app/models/opening_cash.rb
@@ -9,7 +9,10 @@ class OpeningCash < ActiveRecord::Base
 
     validates :monto_efectivo, presence: true, length: {minimum: 4, maximum: 12, :message => "4 dígitos minimo; 12 máximo."}, :numericality => {:only_integer => true, :message => "Solo puede ingresar numeros enteros"}
     validates :fecha_apertura, presence: true
-
+    validates :cash_id, presence: true
+    validates :employee_id, presence: true
+    validates :estado, presence: true
+    
     delegate :descripcion, to: :cash, prefix: true, allow_nil: true
     delegate :nombre, :apellido, :type_of_employee_id, to: :employee, prefix: true, allow_nil: true
 end
