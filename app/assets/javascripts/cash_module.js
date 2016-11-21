42955a8cb0a2c8dd64d555eaa3ddaef30df17e7d
Miguel Carreras <carreras69miguel@gmail.com>
2016-11-21 02:15:46 -0300

    Mejoras en la vista de movimientos y agruegado el modelo cheque

--- app/assets/javascripts/cash_module.js
+++ app/assets/javascripts/cash_module.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     search_invoices();
+    $("#fecha").datepicker();
 });
 
 // cuando selecciono un valor de pago
@@ -39,18 +40,25 @@ function search_invoices(){
 function mostrarCheque(id,detalle){
 	if ($(id).val() == "2") {
     	$(detalle).show();
+        $(detalle).children(".tarjeta").removeAttr("required");
+        $(detalle).children(".tarjeta").val("");
         $(detalle).children(".tarjeta").hide();
+        $(detalle).children(".cheque").prop("required",true);
         $(detalle).children(".cheque").show();
-        mostrarEncabezado();
 
     } else if($(id).val() == "3" || $(id).val() == "4"){
         $(detalle).show();
+        $(detalle).children(".cheque").removeAttr("required");
+        $(detalle).children(".cheque").val("");
         $(detalle).children(".cheque").hide();
-        $(detalle).children(".tarjeta").show();
-        mostrarEncabezado();  	     
+        $(detalle).children(".tarjeta").prop("required",true);
+        $(detalle).children(".tarjeta").show();  	     
     }else{
         $(detalle).hide();
-        mostrarEncabezado();        
+        $(detalle).children(".cheque").val("");
+        $(detalle).children(".tarjeta").val("");
+        $(detalle).children(".cheque").removeAttr("required");
+        $(detalle).children(".tarjeta").removeAttr("required");        
     }
 }
       
--- app/models/payment_type.rb
+++ app/models/payment_type.rb
@@ -1,6 +1,8 @@
 class PaymentType < ActiveRecord::Base
 	belongs_to :cash_movement
-	
+	belongs_to :payment_value
+
+	has_one :check
 	has_many :detail_of_cash_counts
 	
 end
