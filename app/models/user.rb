9a414d650b36a010537a103a70f122d502da7e86
Ariel Sanabria <arielsanabria27@gmail.com>
2016-11-25 16:52:32 -0300

    Validaciones a modelo de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable  
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: { message: "requerido*" }
+  validates_format_of :username, :with => /[a-z]/
+  validates :username, :length => { :minimum => 2, :message => '2 caracteres como minimo*' }
+
+  validates :apellido, presence: { message: "requerido*" }
+  validates_format_of :apellido, :with => /[a-z]/
+  validates :apellido, :length => { :minimum => 2, :message => '2 caracteres como minimo*' }
+
+  validates :numero_ci, presence: { message: "requerido*" }
+  validates :numero_ci, :numericality => { :message => "ingrese valor numerico*" }
+  validates :numero_ci, :length => { :minimum => 7, :maximum => 8, :message => '7 a 8 caracteres como minimo*' }
+  
+  validates :email, presence: { message: "requerido*" }  
   
   belongs_to :rol
     
