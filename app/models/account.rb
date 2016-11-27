988dbd7a43e1893ccd3db9db5d9d3679fa67ca9b
Ariel Sanabria <arielsanabria27@gmail.com>
2016-11-27 15:08:23 -0300

    Restricciones agregadas a facturas y cuentas

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,7 +12,19 @@ class Account < ActiveRecord::Base
     attr_accessor :client
     attr_accessor :room
 
+    before_destroy :change_invoice_state
+
     accepts_nested_attributes_for :account_details, allow_destroy: true , update_only: true
     accepts_nested_attributes_for :room_account_details, allow_destroy: true , update_only: true
+
+    private
+        #Cuando se elimina una cuenta que ya se facturo pero aun no se pago
+        def change_invoice_state
+            factura = Invoice.find_by(account_id: self.id)
+            if !factura.nil?
+                factura.state = "cancelado"
+                factura.save
+            end            
+        end
 
 end
