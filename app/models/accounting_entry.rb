6c492ee87216d16e64aa613e332f54370f82f037
Richard Carreras <richard.carreras.95@gmail.com>
2016-11-27 03:41:09 -0400

    Mejorando vista de Asientos

--- app/models/accounting_entry.rb
+++ app/models/accounting_entry.rb
@@ -1,4 +1,6 @@
 class AccountingEntry < ActiveRecord::Base
 	has_many :account_x_entries
 	has_many :cash_movement
+	attr_accessor :cuenta
+	accepts_nested_attributes_for :account_x_entries, allow_destroy: true , update_only: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161122045023) do
-
+ActiveRecord::Schema.define(version: 20161127064549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +62,7 @@ ActiveRecord::Schema.define(version: 20161122045023) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.string   "tipo"
+    t.string   "account"
   end
 
   add_index "account_x_entries", ["accounting_account_id"], name: "index_account_x_entries_on_accounting_account_id", using: :btree
