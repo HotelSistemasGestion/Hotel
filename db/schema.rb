6bc63bf8158d9cb017dee32d001ced4d0e200e61
Richard Carreras <richard.carreras.95@gmail.com>
2016-11-29 12:22:35 -0400

    Cuentas vista en forma de arbol

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161127064549) do
-
+ActiveRecord::Schema.define(version: 20161128030955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +87,7 @@ ActiveRecord::Schema.define(version: 20161127064549) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "ejercicio"
+    t.integer  "parent_id"
   end
 
   create_table "accounting_entries", force: :cascade do |t|
