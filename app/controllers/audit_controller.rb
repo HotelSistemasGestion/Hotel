cd9d62dea7dba2b4768597f1e4e6e7449d4e77b9
Ariel Sanabria <arielsanabria27@gmail.com>
2016-11-29 13:52:48 -0300

    Cambios en auditoria

--- app/controllers/audit_controller.rb
+++ app/controllers/audit_controller.rb
@@ -1,6 +1,15 @@
 class AuditController < ApplicationController
   before_action :authenticate_user!  
+
   def index
   	@audited_tables = Audited::Audit.all
   end
+
+  def show
+  	@audited_table = Audited::Audit.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
