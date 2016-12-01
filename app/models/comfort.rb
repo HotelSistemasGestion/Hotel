8c0a2402d7f649d382d7f7b7aad2110f5ce288e7
Fernando <fernandoliseras@gmail.com>
2016-11-30 23:08:15 -0300

    agregado filtro por comfort index rooms. close #96

--- app/models/comfort.rb
+++ app/models/comfort.rb
@@ -2,4 +2,7 @@ class Comfort < ActiveRecord::Base
 	has_many :reservation_requests
 	has_many :budgets
 	 belongs_to :room
+  def self.options_for_sorted_by_comfort
+    order('LOWER(descripcion)').map { |e| [e.descripcion, e.id] }
+  end
 end
