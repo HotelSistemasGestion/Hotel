a1c0467d8b2f56d0360e4a806a8341a47deaf3cf
MatiasGonzalezRomero95 <matias.gonzalez95@hotmail.com>
2016-11-30 23:27:42 -0300

    Mejorando validaciones del Modulo Reservas

--- app/models/reservation_request.rb
+++ app/models/reservation_request.rb
@@ -1,14 +1,15 @@
 class ReservationRequest < ActiveRecord::Base
 	belongs_to :comfort
 	has_many :budgets,:dependent => :destroy 
-	validates :nombre, :presence => {:message => "no puede estar en blanco"}
-	validates :apellido, :presence => {:message => "no puede estar en blanco"}
+	validates :nombre, :presence => {:message => "No puede estar en blanco"}
+	validates :apellido, :presence => {:message => "No puede estar en blanco"}
 	validates :email, :presence => {:message => "Ingrese un email"}
-	validates :telefono, :presence => {:message => "ingrese un numero valido"}, :numericality => {:only_integer => true, :message => "solo se aceptan enteros"}
-	validates :cantidad_de_adultos, :presence => {:message => "debe registrar almenos un adulto"}, :numericality => {:only_integer => true, :message => "solo se aceptan enteros"}
+	validates :telefono, :presence => {:message => "Ingrese un numero valido"}
+	validates :cantidad_de_adultos, :presence => {:message => "Debe registrar almenos un adulto"}, :numericality => {:only_integer => true, :message => "solo se aceptan enteros"}
 	###
-	validates :check_in, :presence => {:message => "en blanco"}
-	validates :check_out, :presence => {:message => "en blanco"}
+  validates :comfort_id, :presence => {:message => "No puede estar en blanco"}
+	validates :check_in, :presence => {:message => "No puede estar en blanco"}
+	validates :check_out, :presence => {:message => "No puede estar en blanco"}
 	
 	attr_accessor :date_range
 
