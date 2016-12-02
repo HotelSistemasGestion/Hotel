e3a1a5dcdbbf4fa854d6aad425724e62baf8818d
Richard Carreras <richard.carreras.95@gmail.com>
2016-12-02 00:40:25 -0400

    Mejoras

--- app/controllers/cash_movements_controller.rb
+++ app/controllers/cash_movements_controller.rb
@@ -125,65 +125,9 @@ class CashMovementsController < ApplicationController
         format.html{ redirect_to new_cash_movement_path(opening_cash_id: @cash_movement.opening_cash_id) }
         format.json { render :show, status: :created, location: @cash_movement }
 
-##########################################################################################################
-       @ultimo=AccountingEntry.last.numero + 1
-       @suma = 0
-       invoices.each do |invoice|
-         detalle_factura = InvoiceDetail.where("invoice_id = ?", invoice.id)
-          detalle_factura.each do |detalle| 
-            @asiento=AccountingEntry.new
-            @detalle_asiento=AccountXEntry.new
-            @id_servicio = detalle.service_id
-            @servicio=Service.find(@id_servicio)
-            @monto=detalle.subtotal
-            #@cuenta=AccountingAccount.where("nombre = ?",@servicio.nombre)
-            #if @cuenta == nil
-             #   @nombre_cuenta="Otros"  
-            #end
-            @namem=@servicio.nombre
-            @nombre_cuenta=AccountingAccount.find_by_sql("select accounting_accounts.id from accounting_accounts ,services  where accounting_accounts.nombre='#{@namem}'")   
-            @asiento.numero=@ultimo
-            @asiento.fecha=Time.now.strftime("%d-%m-%Y")
-            @asiento.iva=10
-            @asiento.debe=0
-            @asiento.auto=0
-            @asiento.haber=@monto
-            @asiento.save
-            @suma = @suma + @monto
-            @detalle_asiento.monto=@monto
-            @detalle_asiento.observacion=@servicio.descripcion
-            @detalle_asiento.account=@servicio.nombre
-            @detalle_asiento.accounting_account_id=@nombre_cuenta
-            @detalle_asiento.accounting_entry_id=@ultimo
-            @detalle_asiento.tipo= "D"
-            @detalle_asiento.save
-          end
-        end
-            @masiento=AccountingEntry.new
-            @mdetalle_asiento=AccountXEntry.new
-            @name2="Cliente"
-            @acreedor=AccountingAccount.find_by_sql("select accounting_accounts.id AS id,accounting_accounts.nombre AS nombre from accounting_accounts  where accounting_accounts.nombre='#{@name2}'")   
-            @masiento.numero=@ultimo
-            @masiento.fecha=Time.now.strftime("%d-%m-%Y")
-            @masiento.iva=0
-            @masiento.auto=0
-            @masiento.debe=@suma
-            @masiento.haber=0
-            @masiento.save
-            @mdetalle_asiento.monto=@monto
-            @mdetalle_asiento.observacion="Clientes"
-            @mdetalle_asiento.accounting_entry_id=@ultimo
-            @mdetalle_asiento.tipo= "A"
-            @mdetalle_asiento.account="Cliente"
-            @acreedor.each do |a|
-             @mdetalle_asiento.accounting_account_id=a.id
-            #end 
-            @mdetalle_asiento.save
-            @ultimo=0
-            @suma=0
                       
 ##########################################################################################################
-        ##########################################################################################################
+       
         @ultimo=AccountingEntry.last.numero + 1
         @suma = 0
         invoices.each do |invoice|
